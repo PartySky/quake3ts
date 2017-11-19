2beb6813c9a3c758fe38f9a448fd3670754fe184
Yuri Partitski <PartySky@users.noreply.github.com>
2017-11-19 22:29:47 +0200

    updated gitignor

---
+++ Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace quake3ts
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Set up custom content types -associating file extension to MIME type
+            var provider = new FileExtensionContentTypeProvider();
+            // Add new mappings
+            provider.Mappings[".bsp"] = "x-map";
+            provider.Mappings[".shader"] = "x-shader";
+
+            app.UseDefaultFiles();
+            //app.UseStaticFiles();
+            app.UseStaticFiles(
+                new StaticFileOptions(){ContentTypeProvider = provider}
+            );
+        }
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    watch: true,
+    devtool: 'source-map',
+    entry: './frontend/main.module.ts',
+    module: {
+        rules: [{
+                test: /\.ts?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                loader: "html-loader?exportAsEs6Default"
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".html"]
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'wwwroot/scripts')
+    }
+};
